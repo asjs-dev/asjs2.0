718f96153aada5938c9dffedbc55f4aada266121
Gergely Kormendi <kormendi.gergely@gmail.com>
2017-06-20 05:22:57 +0200

    Change

--- src/js/com/external/Application.js
+++ src/js/com/external/Application.js
@@ -2,6 +2,7 @@ sourcePath( "./" );
 
 includeOnce( "com/external/controller/StartupCommand.js" );
 includeOnce( "com/asjs/model/Language.js" );
+includeOnce( "com/asjs/model/Config.js" );
 
 var Application = createClass( ASJS.Sprite, null, 
 	function( _scope ) {
@@ -15,6 +16,7 @@ var Application = createClass( ASJS.Sprite, null,
 		
 		// private variable
 		var _language = Language.instance();
+		var _config = Config.instance();
 		var _contentView = new ASJS.Sprite();
 		var _styleLoader = new ASJS.StyleLoader();
 		var _languageLoaded = false;
@@ -51,6 +53,8 @@ var Application = createClass( ASJS.Sprite, null,
 		// public function
 		_scope.destruct = function() {
 			_styleLoader.unload();
+			_language.clear();
+			_config.clear();
 		}
 		
 		// protected read only function
